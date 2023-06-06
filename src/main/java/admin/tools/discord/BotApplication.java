60bd0daf26d8f331accd1746e4e1ea17bc6c6870
MirFlames <MirFlames@yandex.ru>
2023-06-06 07:17:41 +0700

    web -> webflux добавил создание и удаление каналов пока черновик

--- src/main/java/admin/tools/discord/BotApplication.java
+++ src/main/java/admin/tools/discord/BotApplication.java
@@ -1,16 +1,9 @@
 package admin.tools.discord;
 
-import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.rest.RestClient;
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.ExitCodeGenerator;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
